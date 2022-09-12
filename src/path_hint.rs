a1e879b39679bd9f8a8acfc6c926d89acbaaa3a8
Praveen Perera <praveen@avencera.com>
2022-09-12 16:55:14 -0500

    Simplify `normalize_path_hint` code a bit

--- src/path_hint.rs
+++ src/path_hint.rs
@@ -26,16 +26,6 @@ pub fn normalize_path_hint(path_hint: String) -> String {
     .into_owned()
 }
 
-fn get_first_capture<'a>(caps: &'a Captures) -> Option<&'a str> {
-    for i in 1..caps.len() {
-        if let Some(c) = caps.get(i) {
-            return Some(c.as_str());
-        }
-    }
-
-    None
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;

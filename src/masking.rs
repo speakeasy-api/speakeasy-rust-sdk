bdd1ad9e27d54b7c023d3d6ecf9ce2abcdf0ad2f
Praveen Perera <praveen@avencera.com>
2022-09-13 17:06:03 -0500

    Swallow errors when setting fields
    - Non user recoverable error
    - User shouldn't be able to create error, if theres an error its a bug we should fix
    - If theres an error we log it

--- src/masking.rs
+++ src/masking.rs
@@ -14,12 +14,6 @@ use self::body_mask::BodyMask;
 pub(crate) const DEFAULT_STRING_MASK: &str = "__masked__";
 pub(crate) const DEFAULT_NUMBER_MASK: i32 = -12321;
 
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("invalid masking option {0}")]
-    BodyMask(#[from] body_mask::Error),
-}
-
 #[derive(Debug, Clone, Default)]
 pub struct Masking {
     response_masks: BodyMask,
@@ -84,11 +78,16 @@ impl Masking {
         &mut self,
         fields: impl Into<Fields>,
         masking_option: impl Into<StringMaskingOption>,
-    ) -> Result<(), Error> {
-        self.request_masks
-            .set_string_field_masks(fields.into(), masking_option.into())?;
-
-        Ok(())
+    ) {
+        if let Err(err) = self
+            .request_masks
+            .set_string_field_masks(fields.into(), masking_option.into())
+        {
+            log::error!(
+                "[SpeakeasySDK Internal error] - invalid request field mask string: {}",
+                err
+            );
+        }
     }
 
     /**
@@ -147,32 +146,47 @@ impl Masking {
         &mut self,
         fields: impl Into<Fields>,
         masking_option: impl Into<NumberMaskingOption>,
-    ) -> Result<(), Error> {
-        self.request_masks
-            .set_number_field_masks(fields.into(), masking_option.into())?;
-
-        Ok(())
+    ) {
+        if let Err(err) = self
+            .request_masks
+            .set_number_field_masks(fields.into(), masking_option.into())
+        {
+            log::error!(
+                "[SpeakeasySDK Internal error] - invalid request field mask string: {}",
+                err
+            );
+        }
     }
 
     pub fn with_response_field_mask_string(
         &mut self,
         fields: impl Into<Fields>,
         masking_option: impl Into<StringMaskingOption>,
-    ) -> Result<(), Error> {
-        self.response_masks
-            .set_string_field_masks(fields.into(), masking_option.into())?;
-
-        Ok(())
+    ) {
+        if let Err(err) = self
+            .response_masks
+            .set_string_field_masks(fields.into(), masking_option.into())
+        {
+            log::error!(
+                "[SpeakeasySDK Internal error] - invalid response field mask string: {}",
+                err
+            );
+        }
     }
 
     pub fn with_response_field_mask_number(
         &mut self,
         fields: impl Into<Fields>,
         masking_option: impl Into<NumberMaskingOption>,
-    ) -> Result<(), Error> {
-        self.response_masks
-            .set_number_field_masks(fields.into(), masking_option.into())?;
-
-        Ok(())
+    ) {
+        if let Err(err) = self
+            .response_masks
+            .set_number_field_masks(fields.into(), masking_option.into())
+        {
+            log::error!(
+                "[SpeakeasySDK Internal error] - invalid response field mask string: {}",
+                err
+            );
+        }
     }
 }

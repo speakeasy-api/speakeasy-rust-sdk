022c7fec6a2c84286435fc0a2673c553340e9373
Praveen Perera <praveen@avencera.com>
2022-09-13 15:46:41 -0500

    Create `Config` and `SpeakeasySDK` structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
-mod body_mask;
+mod masking;
 mod path_hint;
 mod util;
+
+pub type Masking = masking::Masking;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub api_key: String,
+    pub api_id: String,
+    pub version_id: String,
+    pub port: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct SpeakeasySdk {
+    config: Config,
+    masking: Masking,
+}
+
+impl SpeakeasySdk {
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+            masking: Default::default(),
+        }
+    }
+}

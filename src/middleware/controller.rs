a7c232f75e8a42e2a98a8007cd0a22e85e63668d
Praveen Perera <praveen@avencera.com>
2022-09-19 20:20:04 -0500

    Add ability to set path_hint and masking on a per request basis

---
+++ src/middleware/controller.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+use super::{
+    messages::{ControllerMessage, MiddlewareMessage},
+    RequestId,
+};
+use crate::{path_hint, Masking};
+use tokio02::sync::mpsc::Sender;
+
+#[derive(Debug)]
+pub struct State {
+    customer_ids: HashMap<RequestId, String>,
+    path_hints: HashMap<RequestId, String>,
+    masks: HashMap<RequestId, Masking>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            customer_ids: HashMap::new(),
+            path_hints: HashMap::new(),
+            masks: HashMap::new(),
+        }
+    }
+
+    pub(crate) fn handle_message(&mut self, msg: ControllerMessage) {
+        match msg {
+            ControllerMessage::SetMasking {
+                request_id,
+                masking,
+            } => {
+                self.masks.insert(request_id, *masking);
+            }
+            ControllerMessage::SetPathHint {
+                request_id,
+                path_hint,
+            } => {
+                self.path_hints.insert(request_id, path_hint);
+            }
+        }
+    }
+
+    pub(crate) fn get_masking(&mut self, request_id: &RequestId) -> Option<Masking> {
+        self.masks.remove(request_id)
+    }
+}
+
+#[derive(Debug)]
+pub struct Controller {
+    request_id: RequestId,
+    sender: Sender<MiddlewareMessage>,
+}
+
+impl Controller {
+    pub fn new(request_id: RequestId, sender: Sender<MiddlewareMessage>) -> Self {
+        Self { request_id, sender }
+    }
+
+    pub async fn set_path_hint(&self, path_hint: &str) {
+        let path_hint = path_hint::normalize(path_hint);
+
+        self.sender
+            .clone()
+            .send(MiddlewareMessage::ControllerMessage(
+                ControllerMessage::SetPathHint {
+                    request_id: self.request_id.clone(),
+                    path_hint,
+                },
+            ))
+            .await
+            .unwrap();
+    }
+
+    pub async fn set_masking(&self, masking: Masking) {
+        self.sender
+            .clone()
+            .send(MiddlewareMessage::ControllerMessage(
+                ControllerMessage::SetMasking {
+                    request_id: self.request_id.clone(),
+                    masking: Box::new(masking),
+                },
+            ))
+            .await
+            .unwrap();
+    }
+}

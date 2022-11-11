a955d54ec8272fbf07eb6f29b9c490d71ad6c051
Praveen Perera <praveen@avencera.com>
2022-11-11 09:18:22 -0600

    Get protos working for both tokio and tokio02

--- src/speakeasy_protos.rs
+++ src/speakeasy_protos.rs
@@ -1,7 +1,16 @@
 #[cfg(feature = "tokio02")]
 mod tokio02 {
+    mod ingest_service_client;
+
+    pub mod ingest {
+        pub mod ingest_service_client {
+            pub use crate::speakeasy_protos::tokio02::ingest_service_client::IngestServiceClient;
+        }
+
+        pub use speakeasy_protos_tokio_02::ingest::*;
+    }
+
     pub use speakeasy_protos_tokio_02::embedaccesstoken;
-    pub use speakeasy_protos_tokio_02::ingest;
 }
 
 #[cfg(feature = "tokio02")]
@@ -9,8 +18,10 @@ pub use self::tokio02::*;
 
 #[cfg(feature = "tokio")]
 mod tokio {
+    pub mod ingest {
+        pub use speakeasy_protos_tokio_latest::ingest::*;
+    }
     pub use speakeasy_protos_tokio_latest::embedaccesstoken;
-    pub use speakeasy_protos_tokio_latest::ingest;
 }
 
 #[cfg(feature = "tokio")]
---
+++ src/speakeasy_protos/tokio02/ingest_service_client.rs
@@ -0,0 +1,51 @@
+#![doc = r" Generated client implementations."]
+#![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+use speakeasy_protos_tokio_02::ingest::{IngestResponse, IngestRequest};
+use tonic03::codegen::*;
+#[doc = " IngestService is the service definition for the registry ingest endpoint."]
+#[derive(Debug, Clone)]
+pub struct IngestServiceClient<T> {
+    inner: tonic03::client::Grpc<T>,
+}
+impl IngestServiceClient<tonic03::transport::Channel> {
+    #[doc = r" Attempt to create a new client by connecting to a given endpoint."]
+    pub async fn connect<D>(dst: D) -> Result<Self, tonic03::transport::Error>
+    where
+        D: std::convert::TryInto<tonic03::transport::Endpoint>,
+        D::Error: Into<StdError>,
+    {
+        let conn = tonic03::transport::Endpoint::new(dst)?.connect().await?;
+        Ok(Self::new(conn))
+    }
+}
+impl<T> IngestServiceClient<T>
+where
+    T: tonic03::client::GrpcService<tonic03::body::BoxBody>,
+    T::ResponseBody: Body + HttpBody + Send + 'static,
+    T::Error: Into<StdError>,
+    <T::ResponseBody as HttpBody>::Error: Into<StdError> + Send,
+{
+    pub fn new(inner: T) -> Self {
+        let inner = tonic03::client::Grpc::new(inner);
+        Self { inner }
+    }
+    pub fn with_interceptor(inner: T, interceptor: impl Into<tonic03::Interceptor>) -> Self {
+        let inner = tonic03::client::Grpc::with_interceptor(inner, interceptor);
+        Self { inner }
+    }
+    #[doc = "  Ingest is the rpc handling ingest from the SDK."]
+    pub async fn ingest(
+        &mut self,
+        request: impl tonic03::IntoRequest<IngestRequest>,
+    ) -> Result<tonic03::Response<IngestResponse>, tonic03::Status> {
+        self.inner.ready().await.map_err(|e| {
+            tonic03::Status::new(
+                tonic03::Code::Unknown,
+                format!("Service was not ready: {}", e.into()),
+            )
+        })?;
+        let codec = tonic03::codec::ProstCodec::default();
+        let path = http::uri::PathAndQuery::from_static("/ingest.IngestService/Ingest");
+        self.inner.unary(request.into_request(), path, codec).await
+    }
+}
